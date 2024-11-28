84f25ddfd537921c0e68133a9a5143237125e535
Emilly Fernandes <emilly.fernandes0925@gmail.com>
2024-11-28 16:57:05 -0300

    ajustes na tela inicial

--- scripts/nav-scroll.js
+++ scripts/nav-scroll.js
@@ -1,10 +1,11 @@
 function ScrolledTransition(){
     if (window.scrollY >= 100) {
         document.getElementsByTagName('header')[0].classList.add('transparent-header');
-        document.getElementsByClassName('nav-content-left')[0].style.color = 'var(--secondary-color)';
+        document.getElementsByClassName('nav-title')[0].style.color = 'var(--secondary-color)';
+        console.log(document.getElementsByClassName('nav-title')[0]);
     } else {
         document.getElementsByTagName('header')[0].classList.remove('transparent-header');
-        document.getElementsByClassName('nav-content-left')[0].style.color = 'var(--trinary-color)';
+        document.getElementsByClassName('nav-title')[0].style.color = 'var(--trinary-color)';
     }
 };
 
